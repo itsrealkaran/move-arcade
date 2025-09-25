fb665ec0081f4d4bca66e0808ce0f788f87c9a6b
itsrealkaran <karansingh@duck.com>
2025-09-25 11:52:15 +0530

    fix: Configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"
-// import tailwindcss from "@tailwindcss/vite"
+// import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
+
 
 export default defineConfig({
   base: "/",
